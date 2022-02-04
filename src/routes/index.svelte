485b58c66fc832520649ead8b46979fe67cd910d
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-04 18:03:54 -0500

    Initial commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,102 +1,59 @@
-<script context="module">
-	export async function load({ url, fetch }) {
-		const [{ articles, pages }, { tags }] = await Promise.all([
-			fetch(`/articles.json${url.search}`, { credentials: 'include' }).then((r) => r.json()),
-			fetch('/tags.json').then((r) => r.json())
-		]);
-
-		return {
-			props: {
-				articles,
-				pages,
-				tags
-			}
-		};
-	}
+<script context="module" lang="ts">
+	export const prerender = true;
 </script>
 
-<script>
-	import { page, session } from '$app/stores';
-	import ArticleList from '$lib/ArticleList/index.svelte';
-	import Pagination from '$lib/Pagination.svelte';
-
-	export let articles;
-	export let pages;
-	export let tags;
-
-	$: p = +$page.url.searchParams.get('p') || 1;
-	$: tag = $page.url.searchParams.get('tag');
-	$: tab = $page.url.searchParams.get('tab') || 'all';
-	$: page_link_base = tag ? `tag=${tag}` : `tab=${tab}`;
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
-	<title>Conduit</title>
+	<title>Home</title>
 </svelte:head>
 
-<div class="home-page">
-	{#if !$session.user}
-		<div class="banner">
-			<div class="container">
-				<h1 class="logo-font">conduit</h1>
-				<p>A place to share your knowledge.</p>
-			</div>
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
 		</div>
-	{/if}
 
-	<div class="container page">
-		<div class="row">
-			<div class="col-md-9">
-				<div class="feed-toggle">
-					<ul class="nav nav-pills outline-active">
-						<li class="nav-item">
-							<a
-								href="/?tab=all"
-								rel="prefetch"
-								class="nav-link"
-								class:active={tab === 'all' && !tag}
-							>
-								Global Feed
-							</a>
-						</li>
+		to your new<br />SvelteKit app
+	</h1>
 
-						{#if $session.user}
-							<li class="nav-item">
-								<a href="/?tab=feed" rel="prefetch" class="nav-link" class:active={tab === 'feed'}>
-									Your Feed
-								</a>
-							</li>
-						{:else}
-							<li class="nav-item">
-								<a href="/login" rel="prefetch" class="nav-link">Sign in to see your Feed </a>
-							</li>
-						{/if}
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
 
-						{#if tag}
-							<li class="nav-item">
-								<a href="/?tag={tag}" rel="prefetch" class="nav-link active">
-									<i class="ion-pound" />
-									{tag}
-								</a>
-							</li>
-						{/if}
-					</ul>
-				</div>
+	<Counter />
+</section>
 
-				<ArticleList {articles} />
-				<Pagination {pages} {p} href={(p) => `/?${page_link_base}&page=${p}`} />
-			</div>
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
 
-			<div class="col-md-3">
-				<div class="sidebar">
-					<p>Popular Tags</p>
-					<div class="tag-list">
-						{#each tags as tag}
-							<a href="/?tag={tag}" rel="prefetch" class="tag-default tag-pill"> {tag} </a>
-						{/each}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,20 @@
-import netlify from '@sveltejs/adapter-netlify';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: process.env.VERCEL ? vercel() : netlify()
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
+
+export default config;
