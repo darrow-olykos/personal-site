7ad0f4fcf5518a1cceb6aecc1bd096b407dad053
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-04 23:32:30 -0500

    update

--- src/lib/feature-card-grid/FeatureCardGrid.svelte
+++ src/lib/feature-card-grid/FeatureCardGrid.svelte
@@ -18,8 +18,8 @@
 	.container {
 		display: flex;
 		flex-wrap: wrap;
-        gap: 24px;
-        justify-content: center;
+		gap: 24px;
+		justify-content: center;
 	}
 
 	.vertical-stack {
@@ -34,7 +34,7 @@
 		border: 1px var(--primary-color) solid;
 		height: 80px;
 		padding: 10px;
-        width: 392px;
+		width: 392px;
 	}
 
 	.horizontal-stack {
@@ -50,4 +50,50 @@
 		height: 332px;
 		padding: 10px;
 	}
+
+	li:hover {
+		cursor: pointer;
+		box-shadow: 0 0 0 0 rgba(255, 255, 255, 1);
+		animation: grow-white 1.7s, pulse-white 1.7s infinite;
+		animation-fill-mode: forwards;
+		transform: scale(1);
+	}
+
+	@keyframes grow-white {
+		40% {
+			transform: scale(1.06);
+		}
+
+		100% {
+			transform: scale(1.06);
+		}
+	}
+
+    @keyframes pulse-white {
+		40% {
+			box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+		}
+
+		100% {
+			box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
+		}
+	}
+
+    li:active {
+        animation: strong-pulse-white 1s;
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes strong-pulse-white {
+        0% {
+            transform: scale(1.06);
+            box-shadow: 0 0 0 20px rgba(255, 255, 255, .7);
+        }
+
+        100% {
+            transform: scale(1.06);
+            box-shadow: 0 0 0 20px rgba(255, 255, 255, 0);
+        }
+    }
+
 </style>
