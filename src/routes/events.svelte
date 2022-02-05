935080a880a0878fda2be76a96065ecec41ede53
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-05 18:05:28 -0500

    update

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -13,25 +13,55 @@
 
 <section>
 	<div class="upcoming-event">
-		<h2 class="upcoming-event__header">Next Event</h2>
-		<img
-			src="/photos/photo-rust-ferris.jpeg"
-			alt="ferris, the crab mascot of rust, painted in water-color"
-			class="upcoming-event__icon"
-		/>
-		<div class="upcoming-event__group">DC Rust Book Club</div>
-		<div class="upcoming-event__datetime">Thursday, Feb 10th, @7:00pm</div>
-		<div class="upcoming-event__name">DC Rust Book Club: Rust for Rustaceans</div>
-		<div class="upcoming-event__share-button">
-			<Icon icon={shareFill} />
+		<h3 class="upcoming-event__header">Next Event</h3>
+		<div class="upcoming-event__details">
+			<div class="upcoming-event__datetime">Thursday, Feb 10th, @7:00pm</div>
+
+			<div class="upcoming-event__title-row">
+				<div class="upcoming-event__title-container">
+					<div class="upcoming-event__name">DC Rust Book Club: Rust for Rustaceans</div>
+					<div class="upcoming-event__group">DC Rust</div>
+				</div>
+				<img
+					class="upcoming-event__thumbnail"
+					src="/photos/photo-rust-ferris.jpeg"
+					alt="ferris, the crab mascot of rust, painted in water-color"
+				/>
+			</div>
+
+			<div class="upcoming-event__share-button">
+				<Icon icon={shareFill} />
+			</div>
 		</div>
 	</div>
 </section>
 
 <style>
-	.upcoming-event__icon {
-		height: 100px;
-		width: 100px;
+	.upcoming-event {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.upcoming-event__details {
+		border: 1px white solid;
+		padding: 10px;
+		line-height: 24px;
+	}
+
+
+	.upcoming-event__title-row {
+		display: flex;
+		gap: 24px;
+	}
+
+	.upcoming-event__title-container {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.upcoming-event__thumbnail {
+		height: 80px;
+		width: 80px;
 		object-fit: cover;
 		object-position: 100% -15px;
 	}
