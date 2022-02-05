44d6c4fdfca3b086c265628a38eca5f248c150de
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-05 17:30:25 -0500

    update

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -3,6 +3,8 @@
 </script>
 
 <script lang="ts">
+	import Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
+	import shareFill from '@iconify/icons-eva/share-fill';
 </script>
 
 <svelte:head>
@@ -11,23 +13,27 @@
 
 <section>
 	<div class="upcoming-event">
+		<h2 class="upcoming-event__header">Next Event</h2>
 		<img
-			width="400px"
 			src="/photos/photo-rust-ferris.jpeg"
 			alt="ferris, the crab mascot of rust, painted in water-color"
 			class="upcoming-event__icon"
 		/>
-		<div class="upcoming-event__group" />
-		<div class="upcoming-event__datetime" />
-		<div class="upcoming-event__name" />
-		<div class="upcoming-event__share-button" />
+		<div class="upcoming-event__group">DC Rust Book Club</div>
+		<div class="upcoming-event__datetime">Thursday, Feb 10th, @7:00pm</div>
+		<div class="upcoming-event__name">DC Rust Book Club: Rust for Rustaceans</div>
+		<div class="upcoming-event__share-button">
+			<Icon icon={shareFill} />
+		</div>
 	</div>
 </section>
 
 <style>
 	.upcoming-event__icon {
+		height: 100px;
 		width: 100px;
-		object-position: 100% 0;
+		object-fit: cover;
+		object-position: 100% -15px;
 	}
 
 	section {
