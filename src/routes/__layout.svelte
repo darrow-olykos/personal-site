06ce13b7e445d8603b111c093935ab77ef7463dc
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-04 19:27:02 -0500

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,7 +10,8 @@
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	footer
+	<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
 </footer>
 
 <style>
