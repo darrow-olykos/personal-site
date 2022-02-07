52fc6ad28127083d0100f1df866009d48ee792ed
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-07 03:24:17 -0500

    update

--- src/lib/feature-card-grid/FeatureCardGrid.svelte
+++ src/lib/feature-card-grid/FeatureCardGrid.svelte
@@ -3,139 +3,92 @@
 
 <div class="container">
 	<div>
-		What would you like to do?
-		<ul class="horizontal-stack">
-			<li class="left">
+		<div class="title">What would you like to do?</div>
+		<r-grid class="things-to-do-container" columns="4">
+			<r-cell class="action cell1">
 				<a href="/blog">
 					<div class="text">Read Blog</div>
 				</a>
-			</li>
-			<li class="middle-left">
+			</r-cell>
+			<r-cell class="action cell2">
 				<a href="/events">
 					<div class="text">Find Events</div>
 				</a>
-			</li>
-			<li class="middle-right">
+			</r-cell>
+			<r-cell class="action cell3">
 				<a href="/polls">
 					<div class="text">Answer polls</div>
 				</a>
-			</li>
-			<li class="right">
+			</r-cell>
+			<r-cell class="action cell4">
 				<a href="/photos">
 					<div class="text">View photos</div>
 				</a>
-			</li>
-		</ul>
+			</r-cell>
+		</r-grid>
 	</div>
 	<div>
-		Featured
-		<ul class="vertical-stack">
+		<div class="title">Featured</div>
+		<r-grid class="featured-container" columns="1">
 			{#each ['Latest Blog Post: Implementing Visitor Pattern in Rust', 'Upcoming Event: Rust for Rustaceans Chapter 3 Discussion', 'Poll: Recommend Software Engineering Book for Senior Devs?', 'Latest Photo: "Downtown Boston Sunrise"'] as text}
-				<li>{text}</li>
+				<r-cell class="action">{text}</r-cell>
 			{/each}
-		</ul>
+		</r-grid>
 	</div>
 </div>
 
 <style>
-	ul > li {
-		position: relative;
-	}
-
-	.text {
-		color: var(--primary-color);
-		z-index: 3;
-		position: relative;
-		padding-left: 10px;
-		padding-right: 5px;
-		margin-top: 10px;
-		height: 100%;
-	}
-
-	a {
-		text-decoration: none;
-		color: var(--primary-color);
-		position: absolute;
-		height: 100%;
-		width: 100%;
-		top: 0;
-		left: 0;
-		display: block;
-	}
-
 	.container {
 		display: flex;
-		flex-wrap: wrap;
-		gap: 16px;
-		justify-content: center;
+        flex-wrap: wrap;
+        justify-content: center;
+	}
+	.title {
+		margin-bottom: 16px;
 	}
 
-	.horizontal-stack {
-		display: flex;
-		justify-content: start;
-		list-style: none;
-		gap: 16px;
-	}
-
-	.horizontal-stack > * {
-		border: 1px var(--primary-color) solid;
-		width: 108px;
-		height: 376px;
-		padding: 10px;
-	}
-
-	@media (max-width: 500px) {
-		.horizontal-stack {
-			gap: 10px;
-		}
-		.horizontal-stack > * {
-			width: 86px;
-		}
-	}
-
-	.horizontal-stack > .left {
+	/* TODO: is there a less brittle way to accomplish this effect? */
+	.cell1 {
 		background-image: url(/photos/photo-charles-river-bw.jpeg);
 		background-size: cover;
 	}
 
-	.horizontal-stack > .middle-left {
+	.cell2 {
 		background-image: url(/photos/photo-charles-river-bw.jpeg);
 		background-size: cover;
 		background-position-x: -132px;
 	}
 
-	.horizontal-stack > .middle-right {
+	.cell3 {
 		background-image: url(/photos/photo-charles-river-bw.jpeg);
 		background-size: cover;
 		background-position-x: -260px;
 	}
 
-	.horizontal-stack > .right {
+	.cell4 {
 		background-image: url(/photos/photo-charles-river-bw.jpeg);
 		background-size: cover;
 		background-position: right;
 		background-position-x: 142px;
 	}
 
-	.vertical-stack {
-		display: flex;
-		flex-direction: column;
-		justify-content: start;
-		list-style: none;
-		gap: 16px;
-	}
+    .things-to-do-container {
+        margin-bottom: 32px;
+    }
 
-	.vertical-stack > * {
+	.things-to-do-container > .action {
+		position: relative;
 		border: 1px var(--primary-color) solid;
-		height: 65.5px;
+		width: 108px;
+		height: 376px;
 		padding: 10px;
-		width: 380px;
+		margin-right: 16px;
 	}
 
-	.left::after,
-	.middle-left::after,
-	.middle-right::after,
-	.right::after {
+	.cell1::after,
+	.cell2::after,
+	.cell3::after,
+	.cell4::after {
 		content: '';
 		position: absolute;
 		left: 0;
@@ -146,7 +99,7 @@
 		z-index: 2;
 	}
 
-	li:hover {
+	.action:hover {
 		cursor: pointer;
 		box-shadow: 0 0 0 0 rgba(255, 255, 255, 1);
 		animation: grow-white 1.7s, pulse-white 1.7s infinite;
@@ -174,7 +127,7 @@
 		}
 	}
 
-	li:active {
+	.action:active {
 		animation: strong-pulse-white 1s;
 		animation-fill-mode: forwards;
 	}
@@ -188,6 +141,42 @@
 		100% {
 			transform: scale(1.06);
 			box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
+		}
+	}
+
+	.featured-container > * {
+		border: 1px var(--primary-color) solid;
+		height: 65.5px;
+		padding: 10px;
+		width: 380px;
+		margin-bottom: 16px;
+	}
+
+	.text {
+		color: var(--primary-color);
+		z-index: 3;
+		position: relative;
+		padding-left: 10px;
+		padding-right: 5px;
+		margin-top: 10px;
+		height: 100%;
+	}
+
+	a {
+		text-decoration: none;
+		color: var(--primary-color);
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		top: 0;
+		left: 0;
+		display: block;
+	}
+
+	@media (max-width: 600px) {
+		.things-to-do-container > .action {
+			width: 78px;
+			margin-right: 10px;
 		}
 	}
 </style>
