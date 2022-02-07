6219eeae86326b2c7688219a492bef47a1b6aefa
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-06 20:28:39 -0500

    update

---
+++ src/lib/EventCard.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
+	import shareFill from '@iconify/icons-eva/share-fill.js';
+
+    // props
+    export let groupName: string;
+    export let name: string;
+    export let thumbnailUrl: string;
+    export let thumbnailAlt: string;
+    export let datetimeInMs: number;
+
+    // behavior
+    const UPDATE_FREQUENCY_IN_MS = 1000;
+	let todayInMs = Date.now();
+    function updateLatestTime(){
+        todayInMs = Date.now();
+    }
+	setInterval(updateLatestTime, UPDATE_FREQUENCY_IN_MS);
+
+    function formatDatetimeFromMs(ms: number){
+        return new Date(ms).toLocaleDateString(undefined, {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        })
+    }
+
+    // reactive declarations
+	$: daysUntilEvent = Math.round((datetimeInMs - todayInMs) / (1000 * 60 * 60 * 24));
+    $: formattedDatetime = formatDatetimeFromMs(datetimeInMs);
+</script>
+
+<div class="event event--bordered">
+    <div class="event__details">
+        <div class="event__time-until">Starts in {daysUntilEvent} days</div>
+        <div class="event__datetime">{formattedDatetime}</div>
+        <div class="event__title-row">
+            <div class="event__title-container">
+                <div class="event__name">{name}</div>
+                <div class="event__group">{groupName}</div>
+            </div>
+            <img
+                class="event__thumbnail"
+                src={thumbnailUrl}
+                alt={thumbnailAlt}
+            />
+        </div>
+        <div class="event__share-button">
+            <Icon icon={shareFill} />
+        </div>
+    </div>
+</div>
+
+<style>
+	.event {
+		display: flex;
+		flex-direction: column;
+	}
+
+    .event.event--bordered > .event__details {
+        border: 1px white solid;
+    }
+
+	.event__details {
+		padding: 10px;
+		line-height: 24px;
+	}
+
+	.event__details:not(:first-of-type){
+		margin-top: 167px;
+	}
+
+	.event__datetime {
+		color: var(--secondary-color);
+		text-transform: uppercase;
+	}
+
+	.event__time-until {
+		float: right;
+	}
+
+	.event__name {
+		font-family: 'Inconsolata-expanded-bold';
+	}
+
+	.event__title-row {
+		display: flex;
+		gap: 24px;
+	}
+
+	.event__title-container {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.event__thumbnail {
+		height: 80px;
+		width: 80px;
+		object-fit: cover;
+		object-position: 100% -15px;
+	}
+
+	.event__share-button {
+		float: right;
+		padding-bottom: -100px;
+	}
+
+	.event__share-button:hover > :global(svg) {
+		cursor: pointer;
+		box-shadow: 0 0 0 0 rgba(255, 255, 255, 1);
+		animation: pulse-white 1.7s infinite;
+		animation-fill-mode: forwards;
+		transform: scale(1);
+	}
+
+	.event__group {
+		color: gray;
+	}
+
+	@keyframes pulse-white {
+		0% {
+			box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+		}
+
+		100% {
+			box-shadow: 0 0 0 5px rgba(255, 255, 255, 0);
+		}
+	}
+</style>
+
--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -3,14 +3,7 @@
 </script>
 
 <script lang="ts">
-	import Icon from '@iconify/svelte/dist/OfflineIcon.svelte';
-	import shareFill from '@iconify/icons-eva/share-fill.js';
-	let today = Date.now();
-	setInterval(() => {
-		today = Date.now();
-	}, 1000);
-	let upcomingEvent = new Date(2022, 1, 10, 19, 0, 0).getTime();
-	$: timeUntilUpcomingEvent = Math.round((upcomingEvent - today) / (1000 * 60 * 60 * 24));
+	import EventCard from '$lib/EventCard.svelte';
 </script>
 
 <svelte:head>
@@ -18,142 +11,43 @@
 </svelte:head>
 
 <section>
-	<div class="upcoming-event">
+	<div class="event-list">
 		<h3 class="upcoming-event__header">Next Event</h3>
-		<div class="upcoming-event__details">
-			<div class="upcoming-event__time-until">Starts in {timeUntilUpcomingEvent} days</div>
-			<div class="upcoming-event__datetime">Thurs, Feb 10th, @ 7:00 PM EST</div>
-			<div class="upcoming-event__title-row">
-				<div class="upcoming-event__title-container">
-					<div class="upcoming-event__name">Book Club: Rust for Rustaceans</div>
-					<div class="upcoming-event__group">Rust DC</div>
-				</div>
-				<img
-					class="upcoming-event__thumbnail"
-					src="/photos/photo-rust-ferris.jpeg"
-					alt="ferris, the crab mascot of rust, painted in water-color"
-				/>
-			</div>
-			<div class="upcoming-event__share-button">
-				<Icon icon={shareFill} />
-			</div>
-		</div>
+		<EventCard
+			groupName="Rust DC"
+			name="Book Club: Rust for Rustaceans"
+			datetimeInMs={new Date(2022, 1, 10, 19, 0, 0).getTime()}
+			thumbnailUrl="/photos/photo-rust-ferris.jpeg"
+			thumbnailAlt="ferris, the crab mascot of rust, painted with water-color"
+		/>
 	</div>
 
-	<div class="upcoming-event">
+	<div class="event-list">
 		<h3 class="upcoming-event__header">Upcoming Events</h3>
-		<div class="upcoming-event__details">
-			<div class="upcoming-event__time-until">Starts in {timeUntilUpcomingEvent} days</div>
-			<div class="upcoming-event__datetime">Thurs, Feb 10th, @ 7:00 PM EST</div>
-			<div class="upcoming-event__title-row">
-				<div class="upcoming-event__title-container">
-					<div class="upcoming-event__name">Book Club: Rust for Rustaceans</div>
-					<div class="upcoming-event__group">Rust DC</div>
-				</div>
-				<img
-					class="upcoming-event__thumbnail"
-					src="/photos/photo-rust-ferris.jpeg"
-					alt="ferris, the crab mascot of rust, painted in water-color"
-				/>
-			</div>
-			<div class="upcoming-event__share-button">
-				<Icon icon={shareFill} />
-			</div>
-		</div>
-
-		<div class="upcoming-event__details">
-			<div class="upcoming-event__time-until">Starts in {timeUntilUpcomingEvent} days</div>
-			<div class="upcoming-event__datetime">Thurs, Feb 10th, @ 7:00 PM EST</div>
-			<div class="upcoming-event__title-row">
-				<div class="upcoming-event__title-container">
-					<div class="upcoming-event__name">Book Club: Rust for Rustaceans</div>
-					<div class="upcoming-event__group">Rust DC</div>
-				</div>
-				<img
-					class="upcoming-event__thumbnail"
-					src="/photos/photo-rust-ferris.jpeg"
-					alt="ferris, the crab mascot of rust, painted in water-color"
-				/>
-			</div>
-			<div class="upcoming-event__share-button">
-				<Icon icon={shareFill} />
-			</div>
-		</div>
+		<EventCard
+			groupName="Rust DC"
+			name="Book Club: Rust for Rustaceans"
+			datetimeInMs={new Date(2022, 1, 10, 19, 0, 0).getTime()}
+			thumbnailUrl="/photos/photo-rust-ferris.jpeg"
+			thumbnailAlt="ferris, the crab mascot of rust, painted with water-color"
+		/>
+		<EventCard
+			groupName="Rust DC"
+			name="Book Club: Rust for Rustaceans"
+			datetimeInMs={new Date(2022, 1, 10, 19, 0, 0).getTime()}
+			thumbnailUrl="/photos/photo-rust-ferris.jpeg"
+			thumbnailAlt="ferris, the crab mascot of rust, painted with water-color"
+		/>
 	</div>
 </section>
 
 <style>
-	.upcoming-event {
+	.event-list {
 		display: flex;
 		flex-direction: column;
-	}
-
-	.upcoming-event__details {
-		border: 1px white solid;
-		padding: 10px;
-		line-height: 24px;
-	}
-	.upcoming-event__details:not(:first-of-type){
-		margin-top: 16px;
-	}
-
-	.upcoming-event__datetime {
-		color: var(--secondary-color);
-		text-transform: uppercase;
-	}
-
-	.upcoming-event__time-until {
-		float: right;
-	}
-
-	.upcoming-event__name {
-		font-family: 'Inconsolata-expanded-bold';
-	}
-
-	.upcoming-event__title-row {
-		display: flex;
-		gap: 24px;
-	}
-
-	.upcoming-event__title-container {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.upcoming-event__thumbnail {
-		height: 80px;
-		width: 80px;
-		object-fit: cover;
-		object-position: 100% -15px;
-	}
-
-	.upcoming-event__share-button {
-		float: right;
-		padding-bottom: -100px;
-	}
-	.upcoming-event__share-button:hover > :global(svg) {
-		cursor: pointer;
-		box-shadow: 0 0 0 0 rgba(255, 255, 255, 1);
-		animation: pulse-white 1.7s infinite;
-		animation-fill-mode: forwards;
-		transform: scale(1);
-	}
-
-	.upcoming-event__group {
-		color: gray;
 	}
 
 	section {
 		margin-top: 49px;
-	}
-
-	@keyframes pulse-white {
-		0% {
-			box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
-		}
-
-		100% {
-			box-shadow: 0 0 0 5px rgba(255, 255, 255, 0);
-		}
 	}
 </style>
