0e181585e001431145d05f78721392c814017ad1
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-02-08 19:30:07 -0500

    update

---
+++ src/lib/calendar-events/with-recurrences-expanded.ts
@@ -0,0 +1,30 @@
+import type { CalendarEvent } from "./calendar-event";
+
+export function withRecurrencesExpanded(events: Array<CalendarEvent>) {
+    const MAX_NUM_EVENTS = 20;
+    const TIMES_TO_EXPAND = 10;
+    let expandedEvents: Array<CalendarEvent> = [];
+
+    for (let i = 0; i < events.length && expandedEvents.length < MAX_NUM_EVENTS; i++) {
+        let event = events[i];
+        expandedEvents.push(event);
+        if (event.recurrence !== 'none') {
+            let weeksToAdd;
+            const ONE_WEEK_IN_MS = 1000 * 60 * 60 * 24 * 7;
+            if (event.recurrence === 'biweekly') {
+                weeksToAdd = 2;
+            } else if (event.recurrence === 'weekly') {
+                weeksToAdd = 1;
+            } else {
+                throw 'unexpected recurrence value';
+            }
+            for (let expandCount = 1; expandCount <= TIMES_TO_EXPAND; expandCount++) {
+                const eventCopy = Object.assign({}, event);
+                eventCopy.datetimeInMs = event.datetimeInMs + ONE_WEEK_IN_MS * weeksToAdd * expandCount;
+                expandedEvents.push(eventCopy);
+            }
+        }
+    }
+
+    return expandedEvents;
+}
--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -1,47 +1,51 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+export const prerender = true;
 </script>
 
 <script lang="ts">
-	import type { CalendarEvent } from '$lib/calendar-events/calendar-event';
-	import EventCard from '$lib/calendar-events/EventCard.svelte';
+import type { CalendarEvent } from '$lib/calendar-events/calendar-event';
+import EventCard from '$lib/calendar-events/EventCard.svelte';
+import { withRecurrencesExpanded } from '$lib/calendar-events/with-recurrences-expanded';
 
-	let data: Array<CalendarEvent> = [
-		{
-			permaLink: '/events/1',
-			group: {
-                name: 'Rust DC',
-                image: {
-                    url: "/photos/rust-dc-meetup-logo.jpeg",
-                    alt: "rust logo photoshopped onto whitehouse"
-                },
-            },
-			name: 'Book Club: Rust for Rustaceans',
-			datetimeInMs: new Date(2022, 1, 10, 19, 0, 0).getTime(),
-			image: {
-				url: '/photos/photo-rust-ferris.jpeg',
-				alt: 'ferris, the crab mascot of rust, painted with water-color'
-			},
-			recurrence: 'biweekly'
-		},
-		{
-			permaLink: '/events/2',
-            group: {
-                name: 'Rust DC',
-                image: {
-                    url: "/photos/rust-dc-meetup-logo.jpeg",
-                    alt: "rust logo photoshopped onto whitehouse"
-                },
-            },
-			name: 'Mock event name',
-			datetimeInMs: new Date(2022, 2, 11, 20, 0, 0).getTime(),
-			image: {
-				url: '/photos/photo-plant-french-press.jpeg',
-				alt: 'a plant and a french press on my desk'
-			},
-            recurrence: 'none'
-		}
-	];
+let data: Array<CalendarEvent> = [
+    {
+        permaLink: '/events/1',
+        group: {
+            name: 'Rust DC',
+            image: {
+                url: '/photos/rust-dc-meetup-logo.jpeg',
+                alt: 'rust logo photoshopped onto whitehouse'
+            }
+        },
+        name: 'Book Club: Rust for Rustaceans',
+        datetimeInMs: new Date(2022, 1, 10, 19, 0, 0).getTime(),
+        image: {
+            url: '/photos/photo-rust-ferris.jpeg',
+            alt: 'ferris, the crab mascot of rust, painted with water-color'
+        },
+        recurrence: 'biweekly'
+    },
+    {
+        permaLink: '/events/2',
+        group: {
+            name: 'Rust DC',
+            image: {
+                url: '/photos/rust-dc-meetup-logo.jpeg',
+                alt: 'rust logo photoshopped onto whitehouse'
+            }
+        },
+        name: 'Mock event name',
+        datetimeInMs: new Date(2022, 2, 11, 20, 0, 0).getTime(),
+        image: {
+            url: '/photos/photo-plant-french-press.jpeg',
+            alt: 'a plant and a french press on my desk'
+        },
+        recurrence: 'none'
+    }
+];
+
+let events = withRecurrencesExpanded(data);
+
 </script>
 
 <svelte:head>
@@ -49,11 +53,11 @@
 </svelte:head>
 
 <section>
-    {#each data as event}
-	    <div class="container">
-            <EventCard data={event} />
-        </div>
-    {/each}
+	{#each events as event}
+		<div class="container">
+			<EventCard data={event} />
+		</div>
+	{/each}
 </section>
 
 <style>
@@ -61,7 +65,7 @@
 		margin-top: 49px;
 	}
 
-    .container {
-        margin-bottom: 16px;
-    }
+	.container {
+		margin-bottom: 16px;
+	}
 </style>
