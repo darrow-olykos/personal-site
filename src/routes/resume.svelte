5ce6cb48fc08fcb0fea5adc443b881809e4b628a
Darrow O'Lykos <caffeine-yeeter@protonmail.com>
2022-03-31 01:20:13 -0400

    update

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -17,7 +17,9 @@
 </svelte:head>
 
 <section>
+	Click <a href="/no_number_chris_dillinger_resume.pdf" download="no_number_chris_dillinger_resume">here</a> to download resume.
 	<svelte:component this={PdfViewer} url="/no_number_chris_dillinger_resume.pdf" />
+	(The PDF Renderer I'm using does not render in mobile browser)
 </section>
 
 <style>
